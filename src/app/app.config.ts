1ff135899189583bc9ddf359840f243b70400b12
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-03-21 16:36:40 +0200

    Hash

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { NgxSpinnerModule } from "ngx-spinner";
 
 import { routes } from './app.routes';
@@ -11,5 +11,5 @@ import { provideToastr } from 'ngx-toastr';
 import { errorsInterceptor } from './core/interceptors/errors/errors.interceptor';
 import { loadingInterceptor } from './core/interceptors/loading/loading.interceptor';
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),provideAnimations(),provideToastr(),provideHttpClient(withFetch(),withInterceptors([headerInterceptor,errorsInterceptor,loadingInterceptor])),importProvidersFrom(NgxSpinnerModule),provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),provideAnimations(),provideToastr(),provideHttpClient(withFetch(),withInterceptors([headerInterceptor,errorsInterceptor,loadingInterceptor])),importProvidersFrom(NgxSpinnerModule),provideRouter(routes ,withHashLocation()), provideClientHydration(withEventReplay())]
 };
